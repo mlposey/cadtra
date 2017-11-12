c4d3fe7141b1e9f15a100514503c4942f1c85113
Marcus <marcusposey@gmail.com>
2017-11-11 22:28:36 -0600

    Remove unused import

--- client/app/src/main/java/com/marcusposey/cadtra/SignInActivity.java
+++ client/app/src/main/java/com/marcusposey/cadtra/SignInActivity.java
@@ -5,7 +5,6 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.Toast;
 
 import com.google.android.gms.auth.api.Auth;
