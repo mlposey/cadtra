2073e6c70a0bf7e56449ef05b71ebc8b5cd71977
Marcus <marcusposey@gmail.com>
2017-11-12 23:46:43 -0600

    Create id token storage class

---
+++ client/app/src/main/java/com/marcusposey/cadtra/net/TokenStore.java
@@ -0,0 +1,30 @@
+package com.marcusposey.cadtra.net;
+
+import android.app.Activity;
+import android.content.Intent;
+
+import com.marcusposey.cadtra.SignInActivity;
+
+/** Starts SignInActivity to refresh and store an Id Token */
+public class TokenStore extends Activity {
+    private String idToken;
+
+    /** Gets the last token retrieved or a new one if none exists */
+    public String getIdToken() {
+        if (idToken.isEmpty()) refresh();
+        return idToken;
+    }
+
+    /** Attempts to refresh the id token token */
+    public void refresh() {
+        Intent silentSignIn = new Intent(this, SignInActivity.class);
+        silentSignIn.putExtra(SignInActivity.REFRESH_REQUEST, true);
+        startActivityForResult(silentSignIn, 0 /* code doesn't matter */);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        idToken = data.getDataString();
+    }
+}
